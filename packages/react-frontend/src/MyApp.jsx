ab14edbe01ae383a21d876a524b90a9936dfc11d
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-13 09:34:24 -0800

    halfway thru commit (changing IDE) ls
    wq

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,12 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table";
+
+function MyApp() {
+  return (
+    <div className="container">
+      <Table />
+    </div>
+  );
+}
+export default MyApp;
---
+++ packages/react-frontend/src/Table.jsx
@@ -0,0 +1,47 @@
+// src/Table.jsx
+import React from "react";
+
+function TableHeader() {
+  return (
+    <thread>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thread>
+  );
+}
+
+function TableBody() {
+  return (
+      <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+	  <td>Bartender</td>
+	</tr>
+      </tbody>
+  );
+}
+
+function Table() {
+    return(
+      <table>
+	<TableHeader />
+	<TableBody />
+      </table>
+    );
+}
+
+export default Table;
