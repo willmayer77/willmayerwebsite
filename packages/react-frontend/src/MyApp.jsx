370d0a3c0b5cde9748fcbe3996fdb587f91d7bc8
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-13 13:33:58 -0800

    added delete functionality

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,38 @@
 // src/MyApp.jsx
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring Actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
   return (
     <div className="container">
-      <Table />
+      <Table 
+      characterData={characters} 
+      removeCharacter={removeOneCharacter}
+      />
     </div>
   );
 }
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -3,43 +3,46 @@ import React from "react";
 
 function TableHeader() {
   return (
-    <thread>
+    <thead>
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
-    </thread>
+    </thead>
   );
 }
 
-function TableBody() {
-  return (
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-	  <td>Bartender</td>
-	</tr>
-      </tbody>
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key = {index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>
+            Delete 
+          </button>
+        </td>
+      </tr>
+    );
+  }
+);
+  return (  
+    <tbody>
+      {rows}
+    </tbody>
   );
 }
 
-function Table() {
+function Table(props) {
     return(
       <table>
-	<TableHeader />
-	<TableBody />
+	      <TableHeader />
+	      <TableBody 
+        characterData={props.characterData} 
+        removeCharacter={props.removeCharacter}
+        />
       </table>
     );
 }
