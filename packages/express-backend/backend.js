247e59c7be55ed4f886c984232e11dd61c589d1c
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-17 10:03:30 -0800

    get users by name added

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -16,9 +16,22 @@ app.listen(port, () => {
   );
 });
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
+
 app.get("/users", (req, res) => {
-    res.send(users);
-});
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 
 // table data
 const users = {
