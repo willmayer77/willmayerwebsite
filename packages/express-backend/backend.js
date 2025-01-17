fc55062ce000e2c1134ab8a2779d4dab604621d1
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-16 21:02:58 -0800

    added /users route

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -15,3 +15,38 @@ app.listen(port, () => {
     `Example app listening at http://localhost:${port}`
   );
 });
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+
+// table data
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
