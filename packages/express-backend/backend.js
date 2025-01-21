7bdb03038b2d1b3ffcb99517ee0b174e97173dc4
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-21 11:14:06 -0800

    added findbyID functionality

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,6 +33,19 @@ app.get("/users", (req, res) => {
     }
   });
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.")
+    } else {
+        res.send(result);
+    }
+});
+
 // table data
 const users = {
     users_list: [
