d221d9364abf65d04af2afb4ba4d0e707e11f3b3
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-21 22:32:53 -0800

    linking backend changes

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,11 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
