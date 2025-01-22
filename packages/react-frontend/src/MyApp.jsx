1b7ddae925ab72b59a898f58949973b57611d22f
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-21 23:26:49 -0800

    oh my god it works

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -15,27 +15,29 @@ function MyApp() {
         console.log(error);
       })
 }
+
+function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
-
-  function fetchUsers() {
-      const promise = fetch("http://localhost:8000/users");
-      return promise;
   }
 
-  function postUser(person) {
-    const promise = fetch("Http://localhost:8000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(person),
-    });
-
-    return promise;
+function fetchUsers() {
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
   }
 
   useEffect(() => {
@@ -46,7 +48,6 @@ function MyApp() {
   }, [] );
 
 
-  }
   return (
     <div className="container">
       <Table 
