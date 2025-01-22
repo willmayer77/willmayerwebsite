03af3ddcdeb299872e379a3ce8af6005c9eb6d95
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-21 22:34:37 -0800

    frontend changes

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,12 +1,16 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 
-
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
 
   function updateList(person) {
     setCharacters([...characters, person])
@@ -18,6 +22,14 @@ function MyApp() {
     });
     setCharacters(updated);
   }
+
+useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {console.log(error); });
+}, []);
+
   return (
     <div className="container">
       <Table 
