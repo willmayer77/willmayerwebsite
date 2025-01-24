d318163dc3ca1eb445c41928e0ec327812a24275
willmayer77 <145727323+willmayer77@users.noreply.github.com>
2025-01-23 20:17:47 -0800

    its working (fixed add

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -8,6 +8,11 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url}`);
+    next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -29,13 +34,26 @@ const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
 
 const addUser = (user) => {
-    newUser = {
+    var newUser = {
         ...user,
         id: Math.floor(Math.random() * 9999)
     }
     users["users_list"].push(newUser);
+    //add a comment here
+    console.log("made it here too", newUser);
     return newUser;
 };
+
+const deleteUser = (deleteID) => {
+    const index = users.users_list.findIndex(
+        user => user.id === deleteID
+    );
+    if (index !== -1) {
+        users.users_list.splice(index, 1);
+        return 204;
+    }
+    return 404;
+}
 
 
 app.get("/users", (req, res) => {
@@ -61,8 +79,19 @@ app.get("/users/:id", (req, res) => {
 
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
-    addUser(userToAdd);
-    res.status(201).send()
+    const newUser = addUser(userToAdd);
+    res.status(201).send(newUser);
+})
+
+app.delete("/users", (req, res) => {
+    const deleteID = req.body;
+    const status = deleteUser(deleteID);
+    if (status === 204) {
+        res.status(204).send();
+    }
+    else {
+        res.status(404).send();
+    }
 })
 
 // table data
